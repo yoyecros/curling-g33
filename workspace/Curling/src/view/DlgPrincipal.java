97a42a0f66a3dc15fe320f59a83340ef5c058f08
Yohann Cros <yoye.cros@gmail.com>
2014-01-10 11:30:31 +0100

    Fin Sprint 2

--- workspace/Curling/src/view/DlgPrincipal.java
+++ workspace/Curling/src/view/DlgPrincipal.java
@@ -15,6 +15,7 @@ import javax.swing.JFileChooser;
 
 import java.awt.Font;
 import java.io.File;
+import java.io.FileFilter;
 import java.util.Vector;
 
 import javax.swing.SwingConstants;
@@ -32,15 +33,16 @@ public class DlgPrincipal extends JFrame {
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private JTextField textField;
-	private JFileChooser choix;
+	private JFileChooser choixFile, choixFolder;
 	private JButton btnNewButton_1 = new JButton("V\u00E9rifier les URLs...");
+	private int select;
 
 	/**
 	 * Create the frame.
 	 */
 	public DlgPrincipal() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(450, 200, 450, 300);
+		setBounds(450, 200, 550, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -49,67 +51,134 @@ public class DlgPrincipal extends JFrame {
 		JLabel lblNewLabel = new JLabel("Curling");
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel.setFont(new Font("Calibri Light", Font.PLAIN, 20));
-		lblNewLabel.setBounds(10, 11, 414, 26);
+		lblNewLabel.setBounds(50, 11, 414, 26);
 		contentPane.add(lblNewLabel);
 		
 		textField = new JTextField();
+		textField.setEditable(false);
 		textField.setBounds(67, 84, 190, 20);
 		contentPane.add(textField);
 		textField.setColumns(10);
 		
-		JButton btnNewButton = new JButton("S\u00E9lectionner...");
-		listenerChoix ecout = new listenerChoix(this);
-		btnNewButton.addActionListener(ecout);
-		btnNewButton.setBounds(267, 83, 119, 23);
-		contentPane.add(btnNewButton);
+		
+		
+		//--------- boutton file ------------
+		JButton btFile = new JButton("Fichier...");
+		listenerChoixFile ecoutFile = new listenerChoixFile(this);
+		btFile.addActionListener(ecoutFile);
+		btFile.setBounds(267, 83, 87, 23);
+		contentPane.add(btFile);
+		
+		
+		//-------- boutton folder --------------
+		JButton btFolder = new JButton("Dossier...");
+		listenerChoixFolder ecoutFolder = new listenerChoixFolder(this);
+		btFolder.addActionListener(ecoutFolder);
+		btFolder.setBounds(360, 83, 87, 23);
+		contentPane.add(btFolder);
+		
+		
+		
 		
 		btnNewButton_1.setEnabled(false);
 		btnNewButton_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				File f = choix.getSelectedFile();
-				Vector<URLs> tabUrl = CtrlGetURL.getUrlsFromFile(f);
-				if (tabUrl.size() > 0) {
-					Vector<URLs> tabUrlKO = CtrlTestURL.checkListURLs(tabUrl);
-					if(tabUrlKO.isEmpty()) JOptionPane.showMessageDialog(null, "Toutes les URLs sont valides.");
+				if(select == 1) {
+					File f = choixFile.getSelectedFile();
+					Vector<URLs> tabUrl = CtrlGetURL.getUrlsFromFile(f);
+					if (tabUrl.size() > 0) {
+						Vector<URLs> tabUrlKO = CtrlTestURL.checkListURLs(tabUrl);
+						if(tabUrlKO.isEmpty()) JOptionPane.showMessageDialog(null, "Toutes les URLs sont valides.");
+						else {
+							JOptionPane.showMessageDialog(null, "Il y a "+tabUrlKO.size()+" URL(s) non valide(s).");
+							DlgListeURLs list = new DlgListeURLs(tabUrlKO);
+							list.setVisible(true);
+						}
+					}
 					else {
-						JOptionPane.showMessageDialog(null, "Il y a "+tabUrlKO.size()+" URL(s) non valide(s).");
-						DlgListeURLs list = new DlgListeURLs(tabUrlKO);
-						list.setVisible(true);
+						JOptionPane.showMessageDialog(null, "Pas d'URL dans le fichier.");
 					}
 				}
 				else {
-					JOptionPane.showMessageDialog(null, "Pas d'URL dans le fichier.");
+					File filesFolder = choixFolder.getSelectedFile();
+					
+					FileFilter filesFilter = new FileFilter() {
+						public boolean accept(File file) {
+							return !(file.isDirectory());
+						}
+					};
+						
+					File[] filesSelected = filesFolder.listFiles(filesFilter);
+					Vector<URLs> tabUrl = new Vector<URLs>();
+					for(int i=0 ; i<filesSelected.length ; i++) {
+						tabUrl.addAll((CtrlGetURL.getUrlsFromFile(filesSelected[i])));
+					}
+					if (tabUrl.size() > 0) {
+						Vector<URLs> tabUrlKO = CtrlTestURL.checkListURLs(tabUrl);
+						if(tabUrlKO.isEmpty()) JOptionPane.showMessageDialog(null, "Toutes les URLs sont valides.");
+						else {
+							JOptionPane.showMessageDialog(null, "Il y a "+tabUrlKO.size()+" URL(s) non valide(s).");
+							DlgListeURLs list = new DlgListeURLs(tabUrlKO);
+							list.setVisible(true);
+						}
+					}
+					else {
+						JOptionPane.showMessageDialog(null, "Pas d'URL dans le fichier.");
+					}
+					
 				}
+				
 			}
 		});
-		btnNewButton_1.setBounds(123, 158, 183, 23);
+		btnNewButton_1.setBounds(173, 158, 183, 23);
 		contentPane.add(btnNewButton_1);
 	}
 	
-	class listenerChoix implements ActionListener {
+	
+	//------------ file -------------------
+	class listenerChoixFile implements ActionListener {
 		private DlgPrincipal fenetre;
-		public listenerChoix (DlgPrincipal f)
+		public listenerChoixFile (DlgPrincipal f)
 		{
 			this.fenetre = f;
 		}
 		public void actionPerformed(ActionEvent e) {
-			choix = new JFileChooser();
+			choixFile = new JFileChooser();
 			FileNameExtensionFilter filterHTML = new FileNameExtensionFilter(
 					"html", "html");
 			FileNameExtensionFilter filterTXT = new FileNameExtensionFilter(
 					"txt", "txt");
 			FileNameExtensionFilter filterDOCX = new FileNameExtensionFilter(
 					"docx", "docx");
-			choix.setFileFilter(null);
-			choix.addChoosableFileFilter(filterHTML);
-			choix.addChoosableFileFilter(filterTXT);
-			choix.addChoosableFileFilter(filterDOCX);
-			int returnVal = choix.showOpenDialog(null);
+			choixFile.addChoosableFileFilter(filterHTML);
+			choixFile.addChoosableFileFilter(filterTXT);
+			choixFile.addChoosableFileFilter(filterDOCX);
+			int returnVal = choixFile.showOpenDialog(null);
 		    if(returnVal == JFileChooser.APPROVE_OPTION) {
-		       textField.setText(choix.getSelectedFile().getPath());
+		       textField.setText(choixFile.getSelectedFile().getPath());
 		       fenetre.btnNewButton_1.setEnabled(true);
+		       select=1;
 		    }
 		}
+	}
+		//---------------- Folder ----------------
+		class listenerChoixFolder implements ActionListener {
+			private DlgPrincipal fenetre;
+			public listenerChoixFolder (DlgPrincipal f)
+			{
+				this.fenetre = f;
+			}
+			public void actionPerformed(ActionEvent e) {
+				choixFolder = new JFileChooser();
+				choixFolder.setAcceptAllFileFilterUsed(false);
+				choixFolder.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+				int returnVal = choixFolder.showOpenDialog(null);
+			    if(returnVal == JFileChooser.APPROVE_OPTION) {
+			       textField.setText(choixFolder.getSelectedFile().getPath());
+			       fenetre.btnNewButton_1.setEnabled(true);
+			       select=2;
+			    }
+			}
 		
 	}
 	
